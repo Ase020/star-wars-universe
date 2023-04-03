1493cfdacd3f3d10a81a89a87e7ce5780b70135b
felixNyalenda <felixasejunior@gmail.com>
2023-04-03 14:24:50 +0300

    page layout complete

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
    console.log("first commit");
+   fetch("https://www.swapi.tech/api/")
+      .then((res) => res.json())
+      .then((res) => console.log(res));
 });
