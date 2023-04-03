5c92b1200120941e3419f5707074b3e4c9eda17a
felixNyalenda <felixasejunior@gmail.com>
2023-04-03 16:12:19 +0300

    created film list

--- index.js
+++ index.js
@@ -1,6 +1,40 @@
 document.addEventListener("DOMContentLoaded", () => {
-   console.log("first commit");
-   fetch("https://www.swapi.tech/api/")
+   const filmCatalog = document.getElementById("fList");
+   const filmDetails = document.getElementById("film-details");
+
+   function romanConverter(episode) {
+      if (episode === 1) {
+         return "Episode I: ";
+      } else if (episode === 2) {
+         return "Episode II: ";
+      } else if (episode === 3) {
+         return "Episode III: ";
+      } else if (episode === 4) {
+         return "Episode IV: ";
+      } else if (episode === 5) {
+         return "Episode V: ";
+      } else if (episode === 6) {
+         return "Episode VI: ";
+      } else if (episode === 7) {
+         return "Episode VII: ";
+      }
+   }
+
+   //    Fetch film data
+   fetch("https://www.swapi.tech/api/films")
       .then((res) => res.json())
-      .then((res) => console.log(res));
+      .then((data) => {
+         data.result.forEach((film) => {
+            const movieList = document.createElement("li");
+            movieList.className = "li";
+            movieList.textContent = `
+            ${romanConverter(film.properties.episode_id)}${film.properties.title}
+            `;
+            filmCatalog.append(movieList);
+
+            movieList.addEventListener("click", () => {
+               filmDetails.style.display = "flex";
+            });
+         });
+      });
 });
